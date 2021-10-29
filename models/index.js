a4d132172a4facb985488c94d40adad3ae62c1d7
Tony Slonaker <tony.slonaker@gmail.com>
2021-10-28 19:35:15 -0600

    update index

--- models/index.js
+++ models/index.js
@@ -1,3 +1,4 @@
+// import all models 
 const Breed = require('./Breed');
 const Color = require('./Color');
 const Health = require('./Health');
@@ -7,5 +8,31 @@ const Traits = require('./Traits');
 const User = require('./User');
 
 // associations
+Breed.hasMany(Color, {
+    foreignKey: 'color_id'
+});
+
+Breed.hasMany(Health, {
+    foreignKey: 'health_id'
+});
+
+Breed.hasMany(Post, {
+    foreignKey: 'post_id'
+});
+
+Breed.hasMany(Traits, {
+    foreignKey: 'traits_id'
+});
+
+Traits.belongsTo(Breed, {
+    foreignKey: 'traits_id'
+});
+
+
+
+
+
+
+
 
 module.exports = { Breed, Color, Level, Health, Traits, Post, User };
