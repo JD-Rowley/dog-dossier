a198825abe56dc1fe294e9186763dfc9be7a8ac3
JD Rowley <jdrowley1989@gmail.com>
2021-11-04 19:11:17 -0600

    fixed search function

--- public/js/search.js
+++ public/js/search.js
@@ -1,24 +1,9 @@
 async function searchFormHandler(event) {
     event.preventDefault();
 
-    const id = document.querySelector('.option').value;
+    const breed = document.getElementById('breed-selection');
 
-    document.location.replace(`/results/${id}`);
-
-    console.log(id);
-
-    // const response = await fetch(`api/breeds/${id}`, {
-    //     method: 'GET',
-    //     body: JSON.stringify({
-            
-    //     }),
-    //     headers: { 'Content-Type': 'application/json' }
-    // });
-    // if (response.ok) {
-    //     document.location.replace(`results/${id}`);
-    // } else {
-    //     alert(response.statusText);
-    // }
+    document.location.replace(`/results/${breed.value}`);
 };
 
 document.querySelector('.search-button').addEventListener('click', searchFormHandler);
